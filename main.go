cdfbce9c40bc8d5cb2cdaa3b4f8bf317dfbe2e87
eeddaann <edan100@gmail.com>
2023-10-29 20:30:46 +0200

    Add configuration from json file

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
+	"os"
 	"os/exec"
 	"strconv"
 	"time"
@@ -13,10 +15,37 @@ import (
 	"github.com/go-rod/rod/lib/utils"
 )
 
+type Slide struct {
+	Link string `json:"link"`
+	Zoom string `json:"zoom"`
+	Name string `json:"name"`
+}
+
+func takeScreenshot(slide *Slide, browser *rod.Browser) {
+	log.Printf("navigating to: %v", slide.Link)
+	page := browser.MustPage("")
+	var e proto.NetworkResponseReceived
+	page.MustSetExtraHeaders("Authorization", "Basic YWRtaW46YWRtaW4=") // default grafana password
+	page.MustNavigate(slide.Link)
+	wait := page.WaitEvent(&e)
+	wait()
+	page.MustWaitStable()
+	height := page.MustEval(`() => document.body.clientHeight`).String()
+	heightToInt, err := strconv.ParseInt(height, 10, 32)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	page.MustSetViewport(1080, int(heightToInt), 8, false)
+	page.MustEval(`() => document.body.style.zoom =` + slide.Zoom)
+	time.Sleep(5 * time.Second)
+
+	utils.Dump(e.Response.Status, e.Response.URL, e.Response.Headers)
+	log.Printf("response status: %v url: %v", e.Response.Status, e.Response.URL)
+	page.MustScreenshotFullPage(slide.Name + ".png")
+}
+
 func main() {
-	linkFlag := flag.String("url", "http://127.0.0.1:3000", "website url")
 	flag.Parse()
-	link := *linkFlag
 	if path, exists := launcher.LookPath(); exists {
 		log.Printf("using browser found at: %v", path)
 
@@ -27,29 +56,18 @@ func main() {
 		utils.E(cmd.Start())
 		u := launcher.New().Bin(path).Leakless(false).Headless(true).MustLaunch()
 		browser := rod.New().ControlURL(u).MustConnect()
-
 		defer browser.Close()
-		page := browser.MustPage("")
-		var e proto.NetworkResponseReceived
-		page.MustNavigate(link)
-		wait := page.WaitEvent(&e)
-		page.MustSetExtraHeaders("Authorization", "Basic YWRtaW46YWRtaW4=") // default grafana password
-		wait()
-		page.MustWaitStable()
-		height := page.MustEval(`() => document.body.clientHeight`).String()
-		heightToInt, err := strconv.ParseInt(height, 10, 64)
+		content, err := os.ReadFile("conf.json")
 		if err != nil {
-			log.Fatalln(err)
+			log.Fatal(err)
 		}
-		page.MustSetViewport(1080, int(heightToInt), 8, false)
-		page.MustEval(`() => document.body.style.zoom = 0.25`)
-		time.Sleep(5 * time.Second)
-
-		utils.Dump(e.Response.RequestHeaders)
-		log.Printf("request headers: %v", e.Response.RequestHeaders)
-		utils.Dump(e.Response.Status, e.Response.URL, e.Response.Headers)
-		log.Printf("response status: %v url: %v", e.Response.Status, e.Response.URL)
-		page.MustScreenshotFullPage("sample.png")
+		var slides []Slide
+		if err := json.Unmarshal(content, &slides); err != nil {
+			log.Fatal(err)
+		}
+		for _, slide := range slides {
+			takeScreenshot(&slide, browser)
+		}
 
 	}
 }
